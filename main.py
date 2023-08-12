02bba1728826016c7ae5f782d4206870373ceecc
Imanm02 <imanm1381@gmail.com>
2023-08-12 15:17:33 +0330

    add Progress Bar and more Robust Error Handling

--- main.py
+++ main.py
@@ -1,73 +1,76 @@
 import os
-import zipfile
+import sys
+import shutil
+from zipfile import ZipFile, ZIP_DEFLATED
+from tqdm import tqdm
 
 def get_all_file_paths(directory):
-    """
-    Get all file paths in the given directory.
-    
-    :param directory: The root directory to start searching for files.
-    :return: A list of absolute file paths.
-    """
+    """Return a list of file paths for the given directory."""
     file_paths = []
+
+    # os.walk gives us a generator of tuples (dirpath, dirnames, filenames)
     for root, directories, files in os.walk(directory):
         for filename in files:
+
+            # construct full file path
             filepath = os.path.join(root, filename)
             file_paths.append(filepath)
     return file_paths
 
-def create_zip_chunks(directory, output_folder, max_size=1.8*1024*1024*1024):
-    """
-    Zips files into chunks without exceeding the max_size.
-    
-    :param directory: The root directory containing files to be zipped.
-    :param output_folder: The folder where zip files will be saved.
-    :param max_size: The maximum size for each zip file in bytes. Default is 1.8GB.
-    """
-    files = get_all_file_paths(directory)
-    zip_num = 1
-    current_zip_size = 0
-    zip_file = None
-    
-    def start_new_zip():
-        """
-        Starts a new zip file, closes the previous one if it exists.
-        
-        :return: The filename of the new zip file.
-        """
-        nonlocal zip_file, zip_num, current_zip_size
-        if zip_file:
-            zip_file.close()
-        zip_filename = os.path.join(output_folder, f'archive_{zip_num}.zip')
-        zip_file = zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED)
-        zip_num += 1
-        current_zip_size = 0
-        return zip_filename
-    
-    zip_filename = start_new_zip()  # Start the first zip file
+def create_zip_chunks(directory, output_folder):
+    """Create ZIP file chunks from the given directory."""
+    # Define the maximum chunk size as 1.8 GB
+    CHUNK_SIZE = 1.8 * (1024**3)
 
-    for file in files:
-        file_size = os.path.getsize(file)
-        
-        # If adding the next file will exceed the max_size, start a new zip file
-        if current_zip_size + file_size > max_size:
-            zip_filename = start_new_zip()
-        
-        zip_file.write(file, os.path.relpath(file, directory))
-        current_zip_size += file_size
+    # Check if the provided source directory exists
+    if not os.path.exists(directory):
+        print(f"Error: The source directory '{directory}' does not exist.")
+        sys.exit(1)
+
+    # Check if there's enough free space in the output directory
+    free_space = shutil.disk_usage(output_folder).free
+    if free_space < CHUNK_SIZE:
+        print("Error: Not enough disk space on the output directory to create ZIP files.")
+        sys.exit(1)
+
+    # Get the list of all files from the source directory
+    file_paths = get_all_file_paths(directory)
+
+    # Initialize variables
+    zip_file = None
+    current_size = 0
+    chunk_number = 0
     
-    if zip_file:  # Close the last zip file if it's still open
+    # Loop through each file to add to zip
+    for file in tqdm(file_paths, unit="file", desc="Zipping files"):
+        file_size = os.path.getsize(file)
+        # If current file won't fit in current zip chunk, or no chunk is opened yet, open a new chunk
+        if current_size + file_size > CHUNK_SIZE or zip_file is None:
+            # Close the current chunk if it exists
+            if zip_file:
+                zip_file.close()
+
+            # Create a new chunk zip file
+            zip_filename = os.path.join(output_folder, f"archive_part_{chunk_number}.zip")
+            zip_file = ZipFile(zip_filename, 'w', ZIP_DEFLATED)
+            current_size = 0
+            chunk_number += 1
+
+        # Add the current file to the current chunk
+        zip_file.write(file, os.path.relpath(file, directory))
+        current_size += file_size
+
+    # Close the last zip chunk if it exists
+    if zip_file:
         zip_file.close()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
     # Root directory containing files to be zipped
     directory = r'E:\RootDirectory'
     
     # Destination directory for saving the zip files
     output_folder = r'E:\DestinationDirectory'
-    
-    # Create the output directory if it doesn't exist
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-    
-    # Start the zipping process
+
+    # Start the process to create zip chunks
     create_zip_chunks(directory, output_folder)
